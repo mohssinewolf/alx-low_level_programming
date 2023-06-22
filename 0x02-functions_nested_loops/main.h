5a95569ddfb8a4f79f1a067e56363214017a3946
mohssinewolf <heaven-01@live.com>
2023-06-22 04:47:34 -0700

    helo

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+int _putchar(char);
+void print_alphabet (void);
+void print_alphabet_×10(void);
+int _islowerant c)
+int _isalpha int c:
+int print sign(int n);
+int _abs (int);
+int print_last_digit(int);
+void jack_bauer (void); woid times_table (void);
+int add(int, int);
+void print_to_98(int n) ;
+void print_times_table(int n);
+
+#endif
